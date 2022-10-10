4c811b2fe8ee2130b297f9859ffa6880a2ebb9b6
Mystical94 <shubhamgupta6428@gmail.com>
2022-10-11 01:02:48 +0530

    UI for hexxswap

---
+++ UserInterface/forks/@uniswap/sdk/dist/constants.d.ts
@@ -0,0 +1,39 @@
+import JSBI from 'jsbi';
+export declare type BigintIsh = JSBI | bigint | string;
+export declare enum ChainId {
+    MAINNET = 1,
+    ROPSTEN = 3,
+    RINKEBY = 4,
+    GÖRLI = 5,
+    KOVAN = 42,
+    CSC = 53
+}
+export declare enum TradeType {
+    EXACT_INPUT = 0,
+    EXACT_OUTPUT = 1
+}
+export declare enum Rounding {
+    ROUND_DOWN = 0,
+    ROUND_HALF_UP = 1,
+    ROUND_UP = 2
+}
+export declare const FACTORY_ADDRESS = "0x4cAb5791640C439D7Aece517d70BFEfcA4b0Fb6b";
+export declare const INIT_CODE_HASH = "0xc40f9ea891b2e88d18d61c8da1e3cd472823521b8e89a33ca7b50ed7eb7e91a1";
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const TWO: JSBI;
+export declare const THREE: JSBI;
+export declare const FIVE: JSBI;
+export declare const TEN: JSBI;
+export declare const _100: JSBI;
+export declare const _997: JSBI;
+export declare const _1000: JSBI;
+export declare enum SolidityType {
+    uint8 = "uint8",
+    uint256 = "uint256"
+}
+export declare const SOLIDITY_TYPE_MAXIMA: {
+    uint8: JSBI;
+    uint256: JSBI;
+};
